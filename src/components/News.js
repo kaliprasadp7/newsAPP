41097459989532e06129820c8fc3c8609a00d916
KALI PRASAD PANDA <kaliprasaadpanda@KALIs-MacBook-Air.local>
2024-10-21 21:46:32 +0530

    loading spinner added

--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import Spinner from './Spinner';
 
 export class News extends Component {
 
@@ -14,40 +15,49 @@ export class News extends Component {
 
   async componentDidMount(){
     let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=a6901b943dba4084a0996c5696511f28&pageSize=${this.props.pageSize}`;
+    this.setState({loading:true})
     let data = await fetch(url);
     let parsedData = await data.json();
     // console.log(parsedData.articles);
     this.setState({
       articles: parsedData.articles,
-      totalResults : parsedData.totalResults
+      totalResults : parsedData.totalResults,
+      loading:false
     });
   }
 
   handleNext = async () => {
     let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=a6901b943dba4084a0996c5696511f28&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+    this.setState({loading:true})
     let data = await fetch(url);
     let parsedData = await data.json();
     this.setState({
       page : this.state.page + 1, 
-      articles : parsedData.articles});
+      articles : parsedData.articles,
+      loading:false
+    });
   }
 
   handlePrevious = async () => {
     let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=a6901b943dba4084a0996c5696511f28&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+    this.setState({loading:true})
     let data = await fetch(url);
     let parsedData = await data.json();
     this.setState({
       page : this.state.page - 1, 
-      articles : parsedData.articles});
+      articles : parsedData.articles,
+      loading:false});
   }
 
   render() {
     return (
       <div className='container my-3'>
         <h1 className='text-center'>NewsMonkey - Top Headlines</h1>
+        {this.state.loading && <Spinner/>}
         <div className="row mt-4">
           {
-            this.state.articles.map((element) => {
+            //the news will not show when the spinner is running
+            !this.state.loading && this.state.articles.map((element) => {
               return <div className="col-md-3" key={element.url}>
               <NewsItem title={element.title?element.title.slice(0, 42):null} description={element.description?element.description.slice(0, 88):null} imageUrl={element.urlToImage ? element.urlToImage : "https://static.feber.se/article_images/60/22/27/602227.jpg"} newsUrl={element.url} />
             </div>
